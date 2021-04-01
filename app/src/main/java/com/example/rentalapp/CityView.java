8222366bd5db24084083be07c8be591311fbf0a4
Jesse Harasym <51863533+JesseHarasym@users.noreply.github.com>
2021-04-01 13:14:43 -0600

    connected the city search user input to the google maps api location when it fetches. geocodes input city to lat and long and then fetches map. has issue where map closes the first search and then works afterwards.

--- app/src/main/java/com/example/rentalapp/CityView.java
+++ app/src/main/java/com/example/rentalapp/CityView.java
@@ -3,6 +3,8 @@ package com.example.rentalapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,29 +15,61 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+
+import java.util.List;
+import java.util.Locale;
+
 public class CityView extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap cityMap;
+    public String cityName;
+    public double cityLat;
+    public double cityLong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_city_view);
-
         getSupportActionBar().hide();
+
+        setupCitySearch();
+    }
+
+    public void setupCitySearch(){
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            cityName = extras.getString("city");
+        }
+
+        getLocFromName();
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.g_map);
         mapFragment.getMapAsync(this);
     }
 
+    public void getLocFromName(){
+        Geocoder geocoder;
+        List<Address> addresses;
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        try {
+            addresses = geocoder.getFromLocationName(cityName, 5);
+            cityLat = addresses.get(0).getLatitude();
+            cityLong = addresses.get(0).getLongitude();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         cityMap = googleMap;
 
-        //seattle coordinates
-        LatLng calgary = new LatLng(50.98647017296167, -114.05180901726462);
-        cityMap.addMarker(new MarkerOptions().position(calgary).title("Calgary"));
+        LatLng calgary = new LatLng(cityLat, cityLong);
+        cityMap.addMarker(new MarkerOptions().position(calgary).title(cityName));
         cityMap.moveCamera(CameraUpdateFactory.newLatLng(calgary));
         cityMap.setMinZoomPreference(11);
         cityMap.setMaxZoomPreference(18);
@@ -46,15 +80,12 @@ public class CityView extends AppCompatActivity implements OnMapReadyCallback {
         startActivity(page);
     }
 
-    public void showCityMap(){}
-
     public void showPropsOnMap(){}
 
     public void showPropDetails(){}
-
-    public void showMoreDetails(){}
 
     public void viewNextProp(){}
 
     public void viewPrevProp(){}
 }
+
